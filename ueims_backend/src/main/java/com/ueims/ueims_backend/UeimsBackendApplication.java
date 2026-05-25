80b24e027dcfe1a641fa1a04771554aac925bfa8
Nguyen Duy Quang <kingdn522005@gmail.com>
2026-05-25 15:15:33 +0700

    Initial project setup: React frontend and Spring Boot backend

---
+++ ueims_backend/src/main/java/com/ueims/ueims_backend/UeimsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ueims.ueims_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeimsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeimsBackendApplication.class, args);
+	}
+
+}
---
+++ ueims_backend/src/test/java/com/ueims/ueims_backend/UeimsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ueims.ueims_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UeimsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
