74517f62669788b99a4f6fd49afbd3fd85854394
HMSFLASH <166618734+HMSFLASH@users.noreply.github.com>
2026-05-28 13:49:11 +0700

    Delete temp ueims directory unuse

--- ueims/src/main/java/com/swp/ueims/repository/InvalidatedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swp.ueims.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.swp.ueims.entity.InvalidatedToken;
-
-@Repository
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {}
