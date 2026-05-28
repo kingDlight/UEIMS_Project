51293e9d59ec822899dd890c14e7e7aef6ac7559
TranBao198 <lukhachquathoigian198@gmail.com>
2026-05-29 01:08:04 +0700

    feat: Implement student application CV flow and final grade validation

---
+++ ueims_backend/src/main/java/com/ueims/dto/request/ApplicationRequest.java
@@ -0,0 +1,26 @@
+package com.ueims.dto.request;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationRequest {
+    @NotNull(message = "JOB_POST_ID_MANDATORY")
+    private UUID jobPostId;
+
+    @NotNull(message = "STUDENT_ID_MANDATORY")
+    private UUID studentId;
+
+    private String cvFileUrl;
+    private Long cvFileSize;
+    private String coverLetter;
+}
