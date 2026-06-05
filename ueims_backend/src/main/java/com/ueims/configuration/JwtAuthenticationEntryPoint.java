269fc564aed1aed779b72b3f93c8fa15a236935f
HMSFLASH <nangd720@gmail.com>
2026-06-05 23:33:18 +0700

    fix: update error code

--- ueims_backend/src/main/java/com/ueims/configuration/JwtAuthenticationEntryPoint.java
+++ ueims_backend/src/main/java/com/ueims/configuration/JwtAuthenticationEntryPoint.java
@@ -21,6 +21,27 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
             throws IOException, ServletException {
         ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
 
+        Throwable cause = authException.getCause();
+        if (cause instanceof org.springframework.security.oauth2.jwt.JwtException) {
+            String msg = cause.getMessage();
+            if ("Token has been invalidated".equals(msg)) {
+                errorCode = ErrorCode.TOKEN_INVALIDATED;
+            } else if ("Session expired due to inactivity".equals(msg)) {
+                errorCode = ErrorCode.SESSION_EXPIRED;
+            } else if (msg != null && msg.startsWith("Invalid token format")) {
+                errorCode = ErrorCode.INVALID_TOKEN_FORMAT;
+            }
+        } else if (authException.getMessage() != null) {
+            String msg = authException.getMessage();
+            if (msg.contains("Token has been invalidated")) {
+                errorCode = ErrorCode.TOKEN_INVALIDATED;
+            } else if (msg.contains("Session expired due to inactivity")) {
+                errorCode = ErrorCode.SESSION_EXPIRED;
+            } else if (msg.contains("Invalid token format")) {
+                errorCode = ErrorCode.INVALID_TOKEN_FORMAT;
+            }
+        }
+
         response.setHeader("WWW-Authenticate", "Bearer realm=\"UEIMS\"");
         response.setStatus(errorCode.getStatusCode().value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
