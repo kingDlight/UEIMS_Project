1349163fb0a26617a0ed73b061e34f9f926f5f97
TranBao198 <lukhachquathoigian198@gmail.com>
2026-06-10 14:56:57 +0700

    feat: export-excel functionality (fixed BR-20 limit)

--- ueims_backend/src/main/java/com/ueims/controller/AtRiskStudentController.java
+++ ueims_backend/src/main/java/com/ueims/controller/AtRiskStudentController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ueims.model.entity.AtRiskStudent;
 import com.ueims.service.AtRiskStudentService;
@@ -17,10 +20,16 @@ import lombok.RequiredArgsConstructor;
 public class AtRiskStudentController {
 
     private final AtRiskStudentService atRiskStudentService;
+    private final com.ueims.service.ExcelExportService excelExportService;
 
     @GetMapping
     public ResponseEntity<List<AtRiskStudent>> getAtRiskStudents(@RequestParam UUID semesterId) {
         List<AtRiskStudent> atRiskStudents = atRiskStudentService.getAtRiskStudentsBySemester(semesterId);
         return ResponseEntity.ok(atRiskStudents);
+    }
+
+    @GetMapping("/export")
+    public ResponseEntity<byte[]> exportAtRiskStudents(@RequestParam UUID semesterId) {
+        return excelExportService.exportAtRiskStudents(semesterId);
     }
 }
