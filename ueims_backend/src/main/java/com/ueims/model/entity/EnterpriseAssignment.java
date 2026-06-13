8ecd6fc947be2d2e1808e9724a341a9417f31456
HMSFLASH <nangd720@gmail.com>
2026-06-13 20:19:26 +0700

    Fix: Fix HttpMessageConversionException for EnterpriseAssignmentDTO

--- ueims_backend/src/main/java/com/ueims/model/entity/EnterpriseAssignment.java
+++ ueims_backend/src/main/java/com/ueims/model/entity/EnterpriseAssignment.java
@@ -2,6 +2,8 @@ package com.ueims.model.entity;
 
 import jakarta.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.*;
 
 @Entity
@@ -19,14 +21,17 @@ public class EnterpriseAssignment extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "enterprise_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Enterprise enterprise;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private User student;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "semester_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Semester semester;
 
     @Column(name = "status", nullable = false, length = 20)
